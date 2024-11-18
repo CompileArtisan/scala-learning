e9d8d4c9e3c27d7995abb754809cf17567b3a058
CompileArtisan <praanesh.b.nair@gmail.com>
2024-11-18 14:22:19 +0530

    learnt more scala

--- Scala010learningSyntax005lists.scala
+++ Scala010learningSyntax005lists.scala
@@ -28,5 +28,19 @@ object Main {
     val emptyList: List[String] = List.empty[String]
     println(s"Empty list = $emptyList")
 
+    //  cons: adding an element to the beginning of a list
+    val twoThree = List(2, 3)
+    val oneTwoThree = 1 :: twoThree
+    println("oneTwoThree: " + oneTwoThree)
+//    print("TwoThreeOne: " + twoThree :: 1) // No you can't do this
+
+    // tuples
+    val pair = ("Banana", 12, "Apple")
+    println("pair(0) = pair._1 = " + pair._1)
+    println("pair(1) = pair._2 = " + pair._2)
+    println("pair(2) = pair._3 = " + pair._3)
+
+
+
   }
 }
